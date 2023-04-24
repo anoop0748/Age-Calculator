46a497884bc4fe14f8a0e4ce013a2ed612a77543
Anoop Singh <anoop.gada@gmail.com>
2023-04-24 18:52:50 +0530

    Date validation added

--- script.js
+++ script.js
@@ -3,12 +3,33 @@ function calculateAge(){
     let date = +document.getElementById('date').value;
     let month = +document.getElementById('month').value;
     let year = +document.getElementById('year').value;
-    console.log(date,month,year)
+    let dispResult = document.getElementById('result')
+    
     if((date < 1 || date > 31) || (month < 1 || month > 12) || (year < 1000 || year > 9999)){
         document.getElementById('result').innerHTML = "Please Enter valide date , month and year";
         document.getElementById('result').style = "color:red;"
         return 
         
+    }
+    if(month === 2 ){
+        if(date > 28){
+            dispResult.innerHTML = "Please enter valid date. date sholud be >= 28"
+            document.getElementById('result').style = "color:red;"
+            return
+        }
+        else if(year % 4 === 0 && date > 29){
+            dispResult.innerHTML = "Please enter valid date. date should be >= 29"
+            document.getElementById('result').style = "color:red;"
+            return
+        }
+    }
+    if(month === 4 || month === 6 || month === 9 || month === 10){
+        if(date > 30){
+            dispResult.innerHTML = "Please enter valid date, Date should be 1 to 30"
+            document.getElementById('result').style = "color:red;"
+            return
+
+        }
     }
     let currdate = new Date();
     let currDay = currdate.getDate();
